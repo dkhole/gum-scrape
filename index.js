75df4be466ad84e4532cfb196518868386e3bc2f
dkhole <dkhlee419@gmail.com>
2021-04-20 13:17:11 +1000

    working but not optimised

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const listings = [];
 const names = [];
 
 const processListing = async (page, url) => {
+    console.log('>   Navigating to link');
     const resps = await page.goto(url, {waitUntil: 'networkidle2'});
+    console.log('>   Scraping link');
     const bodys = await resps.text();
     const name = $('.seller-profile__name', bodys);
     const breadcrumbs = $('.breadcrumbs__separator', bodys);
@@ -20,24 +22,37 @@ const processListing = async (page, url) => {
 }
 
 const start = async (t0) => {
+    console.log('>   Starting Gum-Scraper');
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+
+    console.log('>   Navigating to gumtree with search restrictions');
     const resp = await page.goto('https://www.gumtree.com.au/s-furniture/waterloo-sydney/furniture/k0c20073l3003798r10?ad=offering', {waitUntil: 'networkidle2'});
+
+    console.log('>   Scraping links');
     const body = await resp.text();
     const result = $('.user-ad-row-new-design', body);
-
     for(let i = 0; i < result.length; i++) {
         listings.push(`https://www.gumtree.com.au${result[i].attribs.href}`);
     }
 
-    await Promise.all(listings.map(async url => {
-        await processListing(page, url);
-    }))
+    console.log('>   Scraping data from individual links');
+    //await processListing(page, listings[0]);
+    //await processListing(page, listings[1]);
+
+    for(let i = 0; i < listings.length; i++) {
+        await processListing(page, listings[i]);
+    }
+
+    /*for(let i = 0; i < listings.length; i++) {
+        await processListing(page, listings[i]);
+    }*/
+
     //end time
     const t1 = performance.now();
     console.log("program took " + (t1 - t0) + " milliseconds.");
 
-    console.log(names);
+    console.table(names);
     await browser.close();
     
 }
