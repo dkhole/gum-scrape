8f08b18bf1a9b4f9ef55805e3dc3aedeb7aa8734
dkhole <dkhlee419@gmail.com>
2021-04-20 14:06:53 +1000

    optimised with puppeteer cluster

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fetch = require('node-fetch');
 const puppeteer = require('puppeteer');
+const { Cluster } = require('puppeteer-cluster');
 const {  performance } = require('perf_hooks');
 const $ = require('cheerio');
 const listings = [];
@@ -7,7 +8,9 @@ const names = [];
 
 const processListing = async (page, url) => {
     console.log('>   Navigating to link');
-    const resps = await page.goto(url, {waitUntil: 'networkidle2'});
+    const resps = await page.goto(url, {timeout: 0, waitUntil: 'domcontentloaded'});
+
+
     console.log('>   Scraping link');
     const bodys = await resps.text();
     const name = $('.seller-profile__name', bodys);
@@ -27,7 +30,7 @@ const start = async (t0) => {
     const page = await browser.newPage();
 
     console.log('>   Navigating to gumtree with search restrictions');
-    const resp = await page.goto('https://www.gumtree.com.au/s-furniture/waterloo-sydney/furniture/k0c20073l3003798r10?ad=offering', {waitUntil: 'networkidle2'});
+    const resp = await page.goto('https://www.gumtree.com.au/s-furniture/waterloo-sydney/furniture/k0c20073l3003798r10?ad=offering', {waitUntil: 'domcontentloaded'});
 
     console.log('>   Scraping links');
     const body = await resp.text();
@@ -40,21 +43,32 @@ const start = async (t0) => {
     //await processListing(page, listings[0]);
     //await processListing(page, listings[1]);
 
-    for(let i = 0; i < listings.length; i++) {
-        await processListing(page, listings[i]);
-    }
-
+    //10 sec slower then concurrent workers
     /*for(let i = 0; i < listings.length; i++) {
         await processListing(page, listings[i]);
     }*/
+    const cluster = await Cluster.launch({
+        concurrency: Cluster.CONCURRENCY_CONTEXT,
+        maxConcurrency: 3,
+    });
 
+    await cluster.task(async ({ page, data: url }) => {
+        await processListing(page, url);
+    });
+
+    for(let i = 0; i < listings.length; i++) {
+        cluster.queue(listings[i]);
+    }
+
+    await cluster.idle();
+    await cluster.close();
     //end time
+
+    await browser.close();
     const t1 = performance.now();
     console.log("program took " + (t1 - t0) + " milliseconds.");
 
     console.table(names);
-    await browser.close();
-    
 }
 //start time
 const t0 = performance.now();
